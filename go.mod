96fcb5f9892d433e71a026047c467858d1567928
Audrius Dailyde <audrius.dailyde@revelsystems.com>
2021-07-16 10:30:28 +0300

    structure

--- go.mod
+++ go.mod
@@ -1,8 +1,5 @@
-module my-project
+module github.com/AudriusDai/lab-go-with-tests
 
 go 1.16
 
-require (
-	github.com/stretchr/testify v1.7.0
-	golang.org/x/tools v0.1.4 // indirect
-)
+require github.com/stretchr/testify v1.7.0
---
+++ hello_world/hello.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const lithuanian = "Lithuanian"
+
+const helloEnglish = "Hello"
+const helloSpanish = "Hola"
+const helloLithuanian = "Labas"
+
+func getGreetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = helloSpanish
+	case lithuanian:
+		prefix = helloLithuanian
+	default:
+		prefix = helloEnglish
+	}
+
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return fmt.Sprintf("%s, %s!", getGreetingPrefix(language), name)
+}
+
+func main() {
+	fmt.Println(Hello("World", ""))
+}
